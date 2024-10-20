e7789976b37f52cfa85cb078d0744dfb59c30221
WellerJoshua98 <wellerjoshua99@gmail.com>
2024-10-20 14:41:12 -0400

    Update course_Controller.py

--- controllers/course_Controller.py
+++ controllers/course_Controller.py
@@ -69,7 +69,54 @@ class CourseController:
         return filtered_array
 
     def getDataFromDWRCS(self):
-        return self.DWRCS 
+        filename = self.DWRCS  # instantiate variable for DegreeWorks file
+        output = []   # array to be returned by method
+        while True:
+            filename = input("\nPlease enter the name of the PDF file from DegreeWorks: ")
+            # check if PdfReader works; check if file exists
+            try:   
+                reader = PdfReader(filename)
+                break   # break while loop
+
+            # file not found; restart loop
+            except:   
+                print("\nInvalid input or file not found. Try again.")
+
+        # creating a pdf reader object
+        reader = PdfReader(filename)
+
+        for i in range(len(reader.pages)):
+            # creating a page object
+            page = reader.pages[i]
+
+            # extracting text from page
+            text = page.extract_text()
+            if i == 0:
+                storeFile = open("convertedText.txt", "w") #creates file if doesn't exist, overwrites if it does
+                storeFile.write(text)
+                storeFile.close()
+            else:
+                storeFile = open("convertedText.txt", "a") #additional pages of PDF as appended to file
+                storeFile.write('\n' + text)
+                storeFile.close()
+        #storeFile (AKA convertedText.txt) is used to easily parse data to be added to output array
+
+        # adds necessary data to array to be processed by other modules in software
+        with open("convertedText.txt", "r") as file:
+            for line in file:
+                # finds lines from list of taken courses and adds them to array; strips \n for better reading
+                if " Fall " in line: 
+                    str = line.strip()
+                    output.append(str)
+                elif " Spring " in line:
+                    str = line.strip()
+                    output.append(str)
+                elif " Summer " in line:
+                    str = line.strip()
+                    output.append(str)
+                
+        file.close()
+        return output   # return array
         
 
 #Testing the getDataFromFYS
